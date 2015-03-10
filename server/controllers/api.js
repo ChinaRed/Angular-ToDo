0cde8ad9c40bdfb7f105980d1a0b0ff2e726e06a
Jon Tan <Jon@jonathans-MacBook-Pro.local>
2015-03-09 14:25:38 -1000

    api.js post delete put

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,8 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var Todo = require('../models/todo');
+var ObjectId = require("mongoose").Types.ObjectId;
 
+// list all todos
 router.get('/', function (req, res) {
-  res.send('Hello Api');
+  Todo.find ( function (err, todos){
+    if (err) throw err;
+    res.json( todos );
+  });
+});
+
+
+// add todo
+router.post('/', function (req, res){
+  Todo.create({ title : req.body.title}, function (err, todo){
+    if (err) throw err;
+    res.json( todo );
+  });
+});
+
+// delete todo
+router.delete('/:id', function (req, res){
+  console.log(req.params.id);
+  Todo.findById(req.params.id)
+    .remove()
+    .exec(function (err, num_removed, status){
+    if (err) throw err;
+    res.json( status ); //this is the todo that was removed
+  });
+});
+
+// complete todo
+router.put('/:id/complete', function (req, res){
+  Todo.update({ _id : ObjectId(req.params.id)}, 
+    { 
+      $set : {
+        completed : true
+      }
+    }, function (err, update_count, result){
+      if (err) throw err;
+      res.json( result ); // this is the todo that was updated
+    });
+});
+
+// uncomplete todo
+router.put('/:id/uncomplete', function (req, res){
+  Todo.update(req.params.id, 
+  {
+    $set : {
+      completed : false
+    }
+  }, function (err, update_count, result){
+    if (err) throw err;
+    res.json( result ); // todo that is waiting to be updated
+  });
 });
 
 module.exports = router;
